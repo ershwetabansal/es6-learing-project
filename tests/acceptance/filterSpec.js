1adcf53e44b4bc66a6772aa4aa80d49debcac3ad
Shweta Bansal <ershwetabansal@gmail.com>
2016-12-22 14:59:15 +0000

    Add a failing test for search in table columns

--- tests/acceptance/filterSpec.js
+++ tests/acceptance/filterSpec.js
@@ -19,7 +19,7 @@ describe("Filter widget", function () {
             target : '#table'
         });
 
-        expect(filter.searchBox().getElementsByTagName('input').length).toBe(2);
+        expect(filter.searchBox().length).toBe(2);
     });
 
     it("sets up a dropdown for filtering if configured", function () {
@@ -75,6 +75,63 @@ describe("Filter widget", function () {
         // Then I see all 5 books
         expect(list.getElementsByTagName('li').length).toBe(4);
 
+    });
+
+    it ("allows to search in first column of a table", function () {
+        document.body.innerHTML =  mock.table;
+        let filter = Filter.setup({
+            target : '#table'
+        });
+
+        var table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(6);
+        var event = new Event('onchange');
+
+        // When I search for Shantaram
+        filter.searchBox()[0].value = 'shantaram';
+        filter.searchBox()[0].dispatchEvent(event);
+
+        // Then I see 2 duplicate records
+        table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(2);
+        expect(table.innerHTML).toContain('Shantaram');
+
+        // When I clear the search
+        filter.searchBox()[0].value = '';
+        filter.searchBox()[0].dispatchEvent(event);
+
+        // Then I see all books
+        table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(6);
+    });
+
+    it("allows to search in the second column of a table", function() {
+        document.body.innerHTML =  mock.table;
+        let filter = Filter.setup({
+            target : '#table'
+        });
+
+        var table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(6);
+        var event = new Event('onchange');
+
+        // When I search for Khalid in the second column
+        filter.searchBox()[1].value = 'Khaled';
+        filter.searchBox()[1].dispatchEvent(event);
+
+        // Then I see 2 records
+        table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(2);
+        expect(table.innerHTML).toContain('Kite runner');
+        expect(table.innerHTML).toContain('A thousand Splendid suns');
+
+        // When I clear the search
+        filter.searchBox()[1].value = '';
+        filter.searchBox()[1].dispatchEvent(event);
+
+        // Then I see all 5 books
+        table = document.getElementById('table').getElementsByTagName('tbody')[0];
+        expect(table.getElementsByTagName('tr').length).toBe(6);
     });
 
     it("allows to search based upon an attribute on each element if configured so", function () {
