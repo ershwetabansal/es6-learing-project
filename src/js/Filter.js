c6d4a6c46eb6bddef14481575fcd956c0440c7bd
Shweta Bansal <ershwetabansal@gmail.com>
2016-12-23 12:36:53 +0000

    Make the tests green

--- src/js/Filter.js
+++ src/js/Filter.js
@@ -1,4 +1,9 @@
-const noop = () => {}
+const noop = () => {};
+
+/**
+ * An abstract class to setup a filter in the DOM.
+ * It can be extended to create a filter for Table, List etc.
+ */
 class Filter {
 
     constructor(options) {
@@ -7,28 +12,31 @@ class Filter {
         this.target = options.target;
         this.cache = document.createElement('div');
         this.cache.innerHTML = this.target.outerHTML;
-        this.searchField = (options.type == 'dropdown') ? document.createElement('select') : document.createElement('input');
     }
 
     static setup(options) {
         (options.target ? noop : () => {throw new Error("Target should be present")})();
 
         options.target = document.querySelector(options.target);
-        return new filterClasses[options.target.tagName + "Filter"](options);
+        if (filterClasses.hasOwnProperty(options.target.tagName + "Filter")) {
+            return new filterClasses[options.target.tagName + "Filter"](options);
+        }
+
+        throw new Error(options.target.tagName + " can not be setup for filtering.");
     }
 
-    find(text, index) {
-        this.target.innerHTML = this.getAllItems()
-            .filter((item) => this.criteria(item, index).toLowerCase().includes(text.toLowerCase()))
+    find() {
+        this.target.innerHTML = this.items()
+            .filter((item) => this.criteria(item))
             .map((item) => (item.outerHTML))
             .join("");
     }
 
     criteria(item) {
-        return item.textContent;
+        return item.textContent.toLowerCase().includes((this.searchBox().value) ? this.searchBox().value.toLowerCase() : '');
     }
 
-    getAllItems() {
+    items() {
         return [];
     }
 
@@ -39,20 +47,21 @@ class Filter {
 
 let filterClasses = {};
 
-filterClasses['ULFilter'] = class  extends Filter {
+filterClasses['ULFilter'] = class extends Filter {
 
     constructor(options) {
         super(options);
+        this.searchField = (options.type == 'dropdown') ? document.createElement('select') : document.createElement('input');
         this.target.parentElement.insertBefore(this.searchField, this.target);
         this.searchField.addEventListener('keyup', (e) => this.find(e.target.value));
     }
 
-    getAllItems() {
-        return [].slice.call(this.cache.getElementsByTagName('li'));
+    items() {
+        return listItems(this.cache);
     }
 };
 
-filterClasses['TABLEFilter'] = class  extends Filter {
+filterClasses['TABLEFilter'] = class extends Filter {
     constructor(options) {
         super(options);
         this.searchField = createSearchTableFor(this.target.getElementsByTagName('tbody')[0]);
@@ -61,22 +70,38 @@ filterClasses['TABLEFilter'] = class  extends Filter {
     }
 
     searchBox() {
-        return cells(this.searchField, 0).map((item) => item.getElementsByTagName('input')[0]);
+        return tableCells(this.searchField, 0).map((item) => item.getElementsByTagName('input')[0]);
     }
 
-    criteria(item, index) {
-        return item.getElementsByTagName('td')[index].textContent;
+    criteria(item) {
+        let allSearches = this.searchBox()
+            .filter((search) => search.value);
+
+        return allSearches.length == 0 || this.searchBox()
+            .filter((search, index) =>
+                search.value &&
+                item.getElementsByTagName('td')[index].textContent.toLowerCase()
+                    .includes(search.value.toLowerCase())
+            )
+            .length == allSearches.length;
     }
 
-    getAllItems() {
-        return [].slice.call(this.cache.getElementsByTagName('tbody')[0].getElementsByTagName('tr'));
+    items() {
+        return tableRows(this.cache);
     }
 };
 
-function cells(table, rowIndex) {
+function tableCells(table, rowIndex) {
     return [].slice.call(table.getElementsByTagName('tr')[rowIndex].getElementsByTagName('td'));
 }
 
+function tableRows(table) {
+    return [].slice.call(table.getElementsByTagName('tbody')[0].getElementsByTagName('tr'));
+}
+
+function listItems(list) {
+    return [].slice.call(list.getElementsByTagName('li'));
+}
 
 function createSearchTableFor(table) {
     let columns = table.getElementsByTagName('tr')[0].getElementsByTagName('td');
